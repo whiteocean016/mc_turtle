b93a8c2ec6ab6148c1f13b64fd5fea899fd9eecd
tomaz <tomaz.belcijan@gmail.com>
2020-05-07 09:45:07 +0200

    update script

--- update.lua
+++ update.lua
@@ -1,6 +1,11 @@
 --[[ Update scripts ]]--
--- Removes script and pulls latest from git
+-- Removes scripts and pulls latest from git
+-- Also updates itself
 
 
 shell.run("rm", "v_exec.lua")
+shell.run("rm", "update.lua")
+
 shell.run("wget", "https://raw.githubusercontent.com/whiteocean016/mc_turtle/master/v_exc.lua")
+shell.run("wget", "https://raw.githubusercontent.com/whiteocean016/mc_turtle/master/update.lua")
+
