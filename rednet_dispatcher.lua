b7b76821406f7004a269e5de2d893b1ecc24fca1
Tomaz Belcijan <tomaz.belcijan@gmail.com>
2020-05-12 16:16:22 +0200

    block choosing

--- rednet_dispatcher.lua
+++ rednet_dispatcher.lua
@@ -1,7 +1,9 @@
 --[[ Rednet dispatcher ]]--
 -- Opens rednet modem and sends commands to excavators
 
-block_coords = {
+block_origin = vector.new(263, 67, 351)
+
+hole_coords = {
     vector.new(0,0,0),
     vector.new(1,0,3),
     vector.new(2,0,1),
@@ -9,12 +11,38 @@ block_coords = {
     vector.new(4,0,2)
 }
 
-block_0 = vector.new(263, 67, 351)
+-- check blocks TODO
+blocks_todo = {}
+for line in io.lines("data_mining/todo.dat") do
+    blocks_todo[#blocks_todo + 1] = line
+end
+
+blocks_done = {}
+for line in io.lines("data_mining/done.dat") do
+    blocks_done[#blocks_done + 1] = line
+end
+
+-- if all done, add new block
+if #blocks_todo == 0 then
+    
+
+-- convert block id to coordinates via pairing function
+function unpair(z)
+    a = math.floor( math.sqrt(z) )
+    if z - math.pow(a,2) < a then
+        return vector.new(z - math.pow(a,2), 0, a)
+    else
+        return vector.new(a, 0, z - math.pow(a,2) - a)
+    end
+end
+
+block_coord = unpair(block_id)
+block_current = block_origin + 5 * block_coord
 
 rednet.open("right")
 for i=1,5 do
     task_done = false
-    vector_exc = block_0 + block_coords[i]
+    vector_exc = block_current + hole_coords[i]
     print("Exacavating: ", vector_exc)
     command = "digg_process " .. vector_exc.x .. " " .. vector_exc.y .. " " .. vector_exc.z
     rednet.broadcast(command)
